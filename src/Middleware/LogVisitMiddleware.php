ee54fa769fa4fe4d3355e0e6b1fb3f7f6796fea2
mkroeders <mkroeders@gmail.com>
2026-09-16 16:30:00 +0200

    Use dbal instead of pdo

--- src/Middleware/LogVisitMiddleware.php
+++ src/Middleware/LogVisitMiddleware.php
@@ -4,36 +4,25 @@ declare(strict_types=1);
 
 namespace LesAbstractSite\Middleware;
 
-use PDO;
 use Override;
 use Throwable;
+use Doctrine\DBAL\Exception;
+use Doctrine\DBAL\Connection;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use LesDatabase\Query\Builder\Applier\Values\InsertValuesApplier;
 
 final class LogVisitMiddleware implements MiddlewareInterface
 {
     private ?Throwable $exception = null;
 
-    private const string INSERT_VISIT_QUERY = <<<'SQL'
-INSERT INTO platform_visitor_raw
-    (
-        platform, page, method, visited_on, http_code, middleware_time, ip, user_agent, preferred_language, referrer, 
-        utm_source, utm_medium, utm_campaign, utm_term, utm_content, error_trace
-     )
-VALUES
-    (
-        :platform, :page, :method, :visited_on, :http_code, :middleware_time, :ip, :user_agent, :preferred_language, :referrer, 
-        :utm_source, :utm_medium, :utm_campaign, :utm_term, :utm_content, :error_trace
-    );
-SQL;
-
     /**
      * @psalm-pure
      */
     public function __construct(
-        private readonly PDO $db,
+        private readonly Connection $db,
         private readonly string $platform,
     ) {}
 
@@ -44,39 +33,51 @@ SQL;
         $response = $handler->handle($request);
         $finish = (int)round(microtime(true));
 
-        $queryParams = $request->getQueryParams();
-
-        $insertVisitStatement = $this->db->prepare(self::INSERT_VISIT_QUERY);
-        $insertVisitStatement->execute(
-            [
-                'platform' => $this->platform,
-                'page' => substr($request->getUri()->getPath(), 0, 255),
-                'method' => substr($request->getMethod(), 0, 10),
-                'visited_on' => round(microtime(true) * 1_000.0),
-                'http_code' => $response->getStatusCode(),
-                'middleware_time' => round(($finish - $start) * 1_000),
-                'ip' => $request->getServerParams()['HTTP_X_REAL_IP'] ?? '',
-                'user_agent' => substr($request->getHeaderLine('HTTP_USER_AGENT'), 0, 255),
-                'preferred_language' => substr($request->getHeaderLine('HTTP_ACCEPT_LANGUAGE'), 0, 255),
-                'referrer' => substr($request->getHeaderLine('HTTP_REFERER'), 0, 255),
-                'utm_source' => $this->getStringParam($queryParams, 'utm_source'),
-                'utm_medium' => $this->getStringParam($queryParams, 'utm_medium'),
-                'utm_campaign' => $this->getStringParam($queryParams, 'utm_campaign'),
-                'utm_term' => $this->getStringParam($queryParams, 'utm_term'),
-                'utm_content' => $this->getStringParam($queryParams, 'utm_content'),
-                'error_trace' => $this->exception
-                    ? sprintf(
-                        "%s:%s = %s\n%s",
-                        $this->exception->getFile(),
-                        $this->exception->getLine(),
-                        $this->exception->getMessage(),
-                        $this->exception->getTraceAsString()
-                    )
-                    : null,
-            ],
-        );
+        $this->insert($request, $response, $start, $finish);
 
         return $response;
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function insert(ServerRequestInterface $request, ResponseInterface $response, int $start, int $finish): void
+    {
+        $serverParams = $request->getServerParams();
+        $queryParams = $request->getQueryParams();
+
+        InsertValuesApplier
+            ::forValues(
+                [
+                    'platform' => $this->platform,
+                    'page' => substr($request->getUri()->getPath(), 0, 255),
+                    'method' => substr($request->getMethod(), 0, 10),
+                    'visited_on' => round(microtime(true) * 1_000.0),
+                    'http_code' => $response->getStatusCode(),
+                    'middleware_time' => round(($finish - $start) * 1_000),
+                    'ip' => $this->getStringParam($serverParams, 'HTTP_X_REAL_IP') ?? '',
+                    'user_agent' => substr($request->getHeaderLine('HTTP_USER_AGENT'), 0, 255),
+                    'preferred_language' => substr($request->getHeaderLine('HTTP_ACCEPT_LANGUAGE'), 0, 255),
+                    'referrer' => substr($request->getHeaderLine('HTTP_REFERER'), 0, 255),
+                    'utm_source' => $this->getStringParam($queryParams, 'utm_source'),
+                    'utm_medium' => $this->getStringParam($queryParams, 'utm_medium'),
+                    'utm_campaign' => $this->getStringParam($queryParams, 'utm_campaign'),
+                    'utm_term' => $this->getStringParam($queryParams, 'utm_term'),
+                    'utm_content' => $this->getStringParam($queryParams, 'utm_content'),
+                    'error_trace' => $this->exception
+                        ? sprintf(
+                            "%s:%s = %s\n%s",
+                            $this->exception->getFile(),
+                            $this->exception->getLine(),
+                            $this->exception->getMessage(),
+                            $this->exception->getTraceAsString()
+                        )
+                        : null,
+                ],
+            )
+            ->apply($this->db->createQueryBuilder())
+            ->insert('platform_visitor_raw')
+            ->executeStatement();
     }
 
     /**
@@ -92,7 +93,7 @@ SQL;
     }
 
     /**
-     * @psalm-impure
+     * @psalm-external-mutation-free
      */
     public function setException(Throwable $e): void
     {
