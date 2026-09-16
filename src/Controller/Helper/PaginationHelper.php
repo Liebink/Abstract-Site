e182d175e203ebab1ab1c00675d13899c7bb4e33
mkroeders <mkroeders@gmail.com>
2026-09-16 22:55:03 +0200

    Pagination helper added

---
+++ src/Controller/Helper/PaginationHelper.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace LesAbstractSite\Controller\Helper;
+
+use Psr\Http\Message\ServerRequestInterface;
+
+// @phpstan-ignore trait.unused
+trait PaginationHelper
+{
+    private function getPaginationPage(ServerRequestInterface $request): ?int
+    {
+        $page = $request->getQueryParams()['page'] ?? null;
+
+        if ($page === null) {
+            return 1;
+        }
+
+        if (is_string($page) && ctype_digit($page) && $page > 1) {
+            return (int) $page;
+        }
+
+        return null;
+    }
+}
---
+++ test/Controller/Helper/PaginationHelperTest.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Controller\Helper;
+
+use Psr\Http\Message\ServerRequestInterface;
+use LesAbstractSite\Controller\Helper\PaginationHelper;
+use PHPUnit\Framework\TestCase;
+
+class PaginationHelperTest extends TestCase
+{
+    public function testNoPageSet(): void
+    {
+        $class = new class {
+            use PaginationHelper {
+                getPaginationPage as public;
+            }
+        };
+
+        $request = $this->createMock(ServerRequestInterface::class);
+
+        self::assertSame(1, $class->getPaginationPage($request));
+    }
+
+    public function testPageSet(): void
+    {
+        $class = new class {
+            use PaginationHelper {
+                getPaginationPage as public;
+            }
+        };
+
+        $request = $this->createMock(ServerRequestInterface::class);
+        $request
+            ->method('getQueryParams')
+            ->willReturn(['page' => '2']);
+
+        self::assertSame(2, $class->getPaginationPage($request));
+    }
+
+    public function testPageSetInvalid(): void
+    {
+        $class = new class {
+            use PaginationHelper {
+                getPaginationPage as public;
+            }
+        };
+
+        $request = $this->createMock(ServerRequestInterface::class);
+        $request
+            ->method('getQueryParams')
+            ->willReturn(['page' => '1']);
+
+        self::assertNull($class->getPaginationPage($request));
+    }
+}
