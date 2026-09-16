903ddbb3d9127d77c99b54c0ad944f33a8ea53bf
mkroeders <mkroeders@gmail.com>
2026-09-16 22:54:53 +0200

    Changed platform into an EnumValueObject, needed for db

--- src/Model/Platform.php
+++ src/Model/Platform.php
@@ -4,8 +4,27 @@ declare(strict_types=1);
 
 namespace LesAbstractSite\Model;
 
-enum Platform: string
+use LesValueObject\Enum\EnumValueObject;
+use LesValueObject\Enum\Helper\EnumValueHelper;
+
+/**
+ * @psalm-immutable
+ */
+enum Platform: string implements EnumValueObject
 {
+    use EnumValueHelper;
+
     case BoekBord = 'BoekBord';
     case GeleefdVerhaal = 'GeleefdVerhaal';
+
+    /**
+     * @psalm-mutation-free
+     */
+    public function getBaseUrl(): string
+    {
+        return match ($this) {
+            self::BoekBord => 'boekbord.nl',
+            self::GeleefdVerhaal => 'geleefdverhaal.nl',
+        };
+    }
 }
